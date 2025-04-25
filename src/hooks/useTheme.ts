a473204ef332cf1c99335bf3325e12b77f4cb37b
metkm <metinkorkmaz417@gmail.com>
2025-04-25 21:10:23 +0300

    remove comments

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,17 +3,15 @@ import { useColorScheme } from 'react-native'
 import { useShallow } from 'zustand/react/shallow'
 
 import { ColorSchemes, defaultColorSchemes } from '@/constants/colors'
-import { getTheme } from '@/stores/lines'
+import { getTheme, useLinesStore } from '@/stores/lines'
 import { useSettingsStore } from '@/stores/settings'
-
-// type SchemeKeys = {
-//   [K in keyof Scheme]: Scheme[K] extends number ? K : never
-// }[keyof Scheme]
 
 export const ColorSchemesContext = createContext<ColorSchemes | undefined>(undefined)
 
 export function useTheme(lineCode?: string) {
   const storedColorScheme = useSettingsStore(useShallow(state => state.colorScheme))
+  const storedTheme = useLinesStore(useShallow(() => lineCode ? getTheme(lineCode) : undefined))
+
   const systemColorScheme = useColorScheme()
 
   const colorScheme = storedColorScheme ?? systemColorScheme ?? 'dark'
@@ -21,40 +19,11 @@ export function useTheme(lineCode?: string) {
 
   const contextTheme = use(ColorSchemesContext) ?? defaultColorSchemes
 
-  const storedTheme = lineCode ? getTheme(lineCode) : undefined
   const schemeColor = lineCode
     ? storedTheme
       ? storedTheme[colorScheme]
       : schemeDefault
     : contextTheme[colorScheme]
-
-  // const schemeColor = useContextScheme ? contextTheme[colorScheme] : defaultColorSchemes[colorScheme]
-
-  // const updateColorTheme = (newPrimaryColor: string) => {
-  //   const argbPrimaryColor = argbFromHex(newPrimaryColor)
-
-  //   const newTheme = themeFromSourceColor(argbPrimaryColor)
-  //   const { neutral } = newTheme.palettes
-
-  //   setColorScheme({
-  //     dark: {
-  //       primary: hexFromArgb(newTheme.schemes['dark']['primary']),
-  //       onPrimary: hexFromArgb(newTheme.schemes['dark']['primary']),
-  //       surface: hexFromArgb(newTheme.schemes['dark']['surface']),
-  //       onSurface: hexFromArgb(newTheme.schemes['dark']['onSurface']),
-  //       surfaceContainer: hexFromArgb(neutral.tone(12)),
-  //       surfaceContainerHigh: hexFromArgb(neutral.tone(17)),
-  //     },
-  //     light: {
-  //       primary: hexFromArgb(newTheme.schemes['light']['primary']),
-  //       onPrimary: hexFromArgb(newTheme.schemes['light']['primary']),
-  //       surface: hexFromArgb(newTheme.schemes['light']['surface']),
-  //       onSurface: hexFromArgb(newTheme.schemes['light']['onSurface']),
-  //       surfaceContainer: hexFromArgb(neutral.tone(94)),
-  //       surfaceContainerHigh: hexFromArgb(neutral.tone(92)),
-  //     }
-  //   })
-  // }
 
   return {
     colorScheme,
@@ -63,44 +32,4 @@ export function useTheme(lineCode?: string) {
     storedTheme,
     contextTheme,
   }
-
-  // const systemScheme = useColorScheme()
-
-  // const contextTheme = useContext(ThemeContext)
-  // const theme = _theme || contextTheme
-
-  // const mode = useMemo(
-  //   () => storedTheme ?? systemScheme ?? 'dark',
-  //   [storedTheme, systemScheme],
-  // )
-
-  // const scheme = useMemo(
-  //   () => {
-  //     const th = theme ?? defaultCreatedTheme
-  //     return mode === 'dark' ? th?.schemes.dark : th?.schemes.light
-  //   },
-  //   [mode, theme],
-  // )
-
-  // const colorsTheme = colors[mode]
-
-  // const bottomSheetStyle = useMemo(() => ({
-  //   handleStyle: { backgroundColor: colorsTheme.surfaceContainerLow },
-  //   handleIndicatorStyle: { backgroundColor: colorsTheme.surfaceContainerHighest },
-  //   backgroundStyle: { backgroundColor: colorsTheme.surfaceContainerLow },
-  // }), [colorsTheme])
-
-  // const getSchemeColorHex = useCallback((key: SchemeKeys) => {
-  //   if (!scheme) return
-  //   return hexFromArgb(scheme[key])
-  // }, [scheme])
-
-  // return {
-  //   mode,
-  //   scheme,
-  //   colorsTheme,
-  //   bottomSheetStyle,
-  //   getSchemeColorHex,
-  //   contextTheme,
-  // }
 }
