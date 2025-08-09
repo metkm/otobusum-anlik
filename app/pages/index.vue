8d751c3cd130b681e2a92205f2c6127a5ee01772
metkm <metinkorkmaz417@gmail.com>
2025-08-09 16:59:06 +0300

    clean

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <div class="-m-2 flex-1">
+  <div class="-m-2 no-top-margin flex-1">
     <ScriptGoogleMaps
-      class="flex grow !w-full !h-full !aspect-auto"
+      class="flex grow !w-full !h-full !aspect-auto hide-copy"
       :center="{ lat: 0, lng: 0 }"
       :map-options="{
         zoom: 4,
@@ -13,3 +13,13 @@
     />
   </div>
 </template>
+
+<style>
+.hide-copy .gmnoprint {
+  display: none;
+}
+
+.no-top-margin {
+  margin-top: calc((var(--safe-area-inset-top) + var(--spacing)) * 2 * -1) !important;
+}
+</style>
