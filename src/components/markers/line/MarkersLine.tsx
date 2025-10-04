a3a47acf439662430033266c1bee9c1a807e1d62
metkm <metinkorkmaz417@gmail.com>
2025-10-04 19:44:10 +0300

    remove react native clusterer for 16kb support

--- src/components/markers/line/MarkersLine.tsx
+++ src/components/markers/line/MarkersLine.tsx
@@ -1,20 +1,21 @@
-import { Platform, View } from 'react-native'
+import { View } from 'react-native'
+// import { Platform, View } from 'react-native'
 import { useShallow } from 'zustand/react/shallow'
 
 import { MarkersBuses } from '../buses/MarkersBuses'
 import { MarkersStop } from '../stop/MarkersStop'
-import { MarkersStopClusteredMemoized } from '../stop/MarkersStopClustered'
+// import { MarkersStopClusteredMemoized } from '../stop/MarkersStopClustered'
 
 import { MarkersLineRouteLine } from './MarkersLineRouteLine'
 
 import { useFiltersStore } from '@/stores/filters'
 import { getLines, useLinesStore } from '@/stores/lines'
 import { useMiscStore } from '@/stores/misc'
-import { useSettingsStore } from '@/stores/settings'
+// import { useSettingsStore } from '@/stores/settings'
 
 export const MarkersLine = () => {
   const invisibleLines = useMiscStore(state => state.invisibleLines)
-  const clusterStops = useSettingsStore(useShallow(state => state.clusterStops))
+  // const clusterStops = useSettingsStore(useShallow(state => state.clusterStops))
 
   useFiltersStore(useShallow(state => state.selectedCity))
   useFiltersStore(useShallow(state => state.selectedGroup))
@@ -28,11 +29,13 @@ export const MarkersLine = () => {
         <View key={lineCode}>
           <MarkersLineRouteLine lineCode={lineCode} />
 
-          {
+          <MarkersStop lineCode={lineCode} />
+
+          {/* {
             clusterStops && Platform.OS !== 'web'
               ? <MarkersStopClusteredMemoized lineCode={lineCode} />
               : <MarkersStop lineCode={lineCode} />
-          }
+          } */}
 
           <MarkersBuses code={lineCode} />
         </View>
