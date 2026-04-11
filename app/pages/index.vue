75c637d37ea7502160cb075bc81d3317c4c6528d
metkm <metinkorkmaz417@gmail.com>
2026-04-11 13:38:58 +0300

    handle read

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
+import type { ShallowRef } from 'vue'
+
 definePageMeta({
   icon: 'i-lucide-map',
   label: 'Map',
 })
+
+const colorMode = useColorMode()
+
+const handleReady = ({ map }: { map: ShallowRef<google.maps.Map | undefined> }) => {
+  map.value?.addListener('center_changed', () => {
+    console.log('new center', map.value?.getCenter())
+  })
+}
 </script>
 
 <template>
   <ScriptGoogleMaps
+    :key="colorMode.value"
     :center="{ lat: -33.8688, lng: 151.2093 }"
     :zoom="12"
     :map-options="{
-      colorScheme: 'DARK',
+      colorScheme: colorMode.value.toUpperCase(),
       cameraControl: false,
       fullscreenControl: false,
       mapTypeControl: false,
@@ -20,5 +31,6 @@ definePageMeta({
       scaleControl: false,
     }"
     class="w-full! flex-1 min-h-0"
+    @ready="handleReady"
   />
 </template>
