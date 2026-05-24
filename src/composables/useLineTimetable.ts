2b889aaa58d5749bbf2ea9990b0051ca0bb875eb
metkm <metinkorkmaz417@gmail.com>
2026-05-24 23:28:42 +0300

    add city back to timetable and stop

--- src/composables/useLineTimetable.ts
+++ src/composables/useLineTimetable.ts
@@ -1,10 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import ky from 'ky'
+import { useShallow } from 'zustand/react/shallow'
 
 import { useLine } from './useLine'
 import { useLineRoutes } from './useLineRoutes'
 
 import { CACHE_MS_2_WEEK } from '@/constants/app'
+import { useFilterStore } from '@/stores'
 
 export type Time = `${number}:${number}:${number}`
 
@@ -21,12 +23,14 @@ interface Timetable {
 export const useLineTimetable = () => {
   const { direction } = useLineRoutes()
   const { code } = useLine()
+  const city = useFilterStore(useShallow(state => state.city))
 
   const query = useQuery({
     queryKey: ['timetable', code, direction],
     queryFn: () => ky.get<Timetable>(`${process.env.EXPO_PUBLIC_BASE_URL}/timetable/${code}`, {
       searchParams: {
         direction,
+        city,
       },
     }).json(),
     staleTime: CACHE_MS_2_WEEK,
--- src/composables/useStop.ts
+++ src/composables/useStop.ts
@@ -1,12 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
 import ky from 'ky'
+import { useShallow } from 'zustand/react/shallow'
 
+import { useFilterStore } from '@/stores'
 import { BusStopWithBuses } from '@/types/bus'
 
 export const useStop = (stopCode: string | number) => {
+  const city = useFilterStore(useShallow(state => state.city))
+
   const query = useQuery({
     queryKey: ['stop', stopCode],
-    queryFn: () => ky.get<BusStopWithBuses>(`${process.env.EXPO_PUBLIC_BASE_URL}/v1/stop/${stopCode}`).json(),
+    queryFn: () => ky.get<BusStopWithBuses>(`${process.env.EXPO_PUBLIC_BASE_URL}/v1/stop/${stopCode}`, {
+      searchParams: {
+        city,
+      },
+    }).json(),
     staleTime: 86_400_000,
     meta: { persist: true },
   })
