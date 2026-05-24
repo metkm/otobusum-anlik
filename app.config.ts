5ba4c0ac0ca784618b1c6bb7cbe21d8e299088d3
metkm <metinkorkmaz417@gmail.com>
2026-05-25 00:03:12 +0300

    bump

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext) => ({
   ...config,
   name: 'Otobüsüm Anlık',
   slug: 'otobusum-anlik',
-  version: '1.4.4',
+  version: '1.4.5',
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   scheme: 'otobusum-anlik',
