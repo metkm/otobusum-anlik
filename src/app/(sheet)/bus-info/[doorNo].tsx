db3037910d5ef2e9a4d676c323bcd43daeca0a23
metkm <metinkorkmaz417@gmail.com>
2026-06-03 22:38:44 +0300

    add icons to bus info page

--- src/app/(sheet)/bus-info/[doorNo].tsx
+++ src/app/(sheet)/bus-info/[doorNo].tsx
@@ -1,12 +1,31 @@
 import { useLocalSearchParams } from 'expo-router'
 import { t } from 'i18next'
+import { ComponentProps } from 'react'
 import { View } from 'react-native'
 
 import { UActivityIndicator } from '@/components/u/UActivityIndicator'
+import { UIcon } from '@/components/u/UIcon'
 import { UQueryState } from '@/components/u/UQueryState'
 import { UText } from '@/components/u/UText'
 
 import { useLineBusInfo } from '@/composables/useLineBusInfo'
+
+const BusInfoItem = ({ label, value, icon }: { label: string, value?: string | number, icon: ComponentProps<typeof UIcon>['name'] }) => {
+  return (
+    <View className="gap-1">
+      <View className="flex-row gap-1">
+        <UIcon
+          name={icon}
+          colorClassName="text-default"
+        />
+
+        <UText className="text-xs font-inter-medium text-muted">{label}</UText>
+      </View>
+
+      <UText>{value}</UText>
+    </View>
+  )
+}
 
 const BusInfo = () => {
   const searchParams = useLocalSearchParams()
@@ -16,32 +35,37 @@ const BusInfo = () => {
 
   return (
     <View className="pt-5 px-2 pb-2 gap-2">
-      <View>
-        <UText className="text-xs font-inter-medium text-muted">{t('doorNo')}</UText>
-        <UText>{doorNo}</UText>
-      </View>
+      <BusInfoItem
+        label={t('doorNo')}
+        value={doorNo}
+        icon="door-closed"
+      />
 
       <UQueryState query={query} loading={() => <UActivityIndicator />}>
-        <View>
-          <UText className="text-xs font-inter-medium text-muted">{t('operator')}</UText>
-          <UText>{query.data?.operator}</UText>
-        </View>
+        <BusInfoItem
+          label={t('operator')}
+          value={query.data?.operator}
+          icon="circle-user"
+        />
 
-        <View>
-          <UText className="text-xs font-inter-medium text-muted">{t('plate')}</UText>
-          <UText>{query.data?.plate}</UText>
-        </View>
+        <BusInfoItem
+          label={t('plate')}
+          value={query.data?.plate}
+          icon="bandage"
+        />
 
-        <View>
-          <UText className="text-xs font-inter-medium text-muted">{t('speed')}</UText>
-          <UText>{`${query.data?.speed} km/h`}</UText>
-        </View>
+        <BusInfoItem
+          label={t('speed')}
+          value={`${query.data?.speed} km/h`}
+          icon="gauge"
+        />
 
         {query.data?.garage && (
-          <View>
-            <UText className="text-xs font-inter-medium text-muted">{t('garage')}</UText>
-            <UText>{query.data?.garage}</UText>
-          </View>
+          <BusInfoItem
+            label={t('garage')}
+            value={query.data?.garage}
+            icon="van"
+          />
         )}
       </UQueryState>
     </View>
