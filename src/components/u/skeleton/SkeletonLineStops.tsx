542c00722305481b632cb437120572baf49423f2
metkm <metinkorkmaz417@gmail.com>
2026-06-08 12:00:44 +0300

    fix skeleton for bus stops loader

--- src/components/u/skeleton/SkeletonLineStops.tsx
+++ src/components/u/skeleton/SkeletonLineStops.tsx
@@ -4,12 +4,12 @@ import { SkeletonPulse } from './SkeletonPulse'
 
 export const SkeletonLineStops = () => {
   return (
-    <View className="h-28 overflow-hidden gap-2 px-2">
+    <View className="h-28 overflow-hidden gap-1 px-2">
       {[1, 2, 3].map(i => (
         <View key={i} className="flex-row items-center gap-2">
-          <SkeletonPulse className="h-6 w-6.5" />
-          <SkeletonPulse className="rounded-full size-10 border-2 bg-transparent border-black/50" />
-          <SkeletonPulse className="h-6 grow" />
+          <SkeletonPulse className="h-4 w-6.5" />
+          <SkeletonPulse className="rounded-full size-8 border-2 bg-transparent border-black/50" />
+          <SkeletonPulse className="h-4 grow" />
         </View>
       ))}
     </View>
